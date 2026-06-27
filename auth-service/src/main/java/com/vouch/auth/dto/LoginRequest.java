257208c0720be80f85c27a4e6374a6c3e829e55e
Leji Darke <lejidarke@gmail.com>
2026-06-27 22:17:30 +0000

    feat: add email login support to auth-service

--- auth-service/src/main/java/com/vouch/auth/dto/LoginRequest.java
+++ auth-service/src/main/java/com/vouch/auth/dto/LoginRequest.java
@@ -1,14 +1,11 @@
 package com.vouch.auth.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class LoginRequest {
-
-    @NotBlank(message = "Phone number is required")
-    private String phone;
-
-    @NotBlank(message = "Password is required")
+    // Can be either a phone number or email address
+    private String identifier;
+    private String loginMethod; // "phone" or "email"
     private String password;
 }
